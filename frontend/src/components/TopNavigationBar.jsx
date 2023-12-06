a1e0a3ccfcc0658ac7f066a0d745780b3f5357df
GwMamba <gerald.mwang@gmail.com>
2023-12-06 00:37:44 -0700

    Top nav bar component

--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
-
 import '../styles/TopNavigationBar.scss'
 import FavBadge from './FavBadge';
 import TopicList from './TopicList';
 
 const TopNavigation = (props) => {
 
-
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
-      <TopicList topics={props.topics} topicSelect={props.topicSelect}/>
+      <TopicList topicSelect={props.topicSelect} topics={props.topics} />
       <FavBadge favorites={props.favorites} selected={props.favorites} />
     </div>
   )
