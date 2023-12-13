d51790055238216eab6cea96cfe1d48b61afdc1b
GwMamba <gerald.mwang@gmail.com>
2023-12-14 05:44:44 +0900

    Refactored code.

--- frontend/src/components/FavBadge.jsx
+++ frontend/src/components/FavBadge.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import FavIcon from './FavIcon';
 import '../styles/FavBadge.scss';
 
-const FavBadge = ({ showNotification }) => {
+const FavBadge = (props) => {
   
   return (
     <div className='fav-badge'>
-      <FavIcon className='fav-badge__count ' displayAlert={showNotification} />
+      <FavIcon displayAlert={props.favorites} selected={true} />
     </div>
   ) 
 };
